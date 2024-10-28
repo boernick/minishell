823f5f5127c3d58bb465e14137121378562899f9
boernick <nick.boer@hotmail.com>
2024-10-28 23:10:51 +0100

    update2810

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: nick <nick@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 19:17:53 by nboer             #+#    #+#             */
-/*   Updated: 2024/10/28 22:10:43 by nick             ###   ########.fr       */
+/*   Updated: 2024/10/28 23:10:44 by nick             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,19 @@
 # include <sys/wait.h>
 # include "../includes/Libft/libft.h"
 
+// if one single struct will be too messy or data is often irrelevant in parsing/exec we could separate between token struct and execution struct
 typedef struct s_data
 {
-	pid_t		pid; // process ID number, if 0 -> process = child
-	int			fd_in; // FD in for pipe
-	int			fd_out; // FD out for pipe
-	int			tag_out; // numbertag that indicates whether the outfile is read/write/append
-	int			p_exit; //expand latest exit status of the most recently executed foreground pipe. (case $?)
+	pid_t		ex_pid; // process ID number, if 0 -> process = child
+	int			ex_fdin; // FD in for pipe
+	int			ex_fdout; // FD out for pipe
+	int			ex_tag_out; // numbertag that indicates whether the outfile is read/write/append
+	int			ex_p_exit; //expand latest exit status of the most recently executed foreground pipe. (case $?)
 	char*		arg; // or cmd?
+	char*		envp; //or char**?
 }	t_data;
 
 /* TOKENIZATION */
-char* function_xx();
 
 /* PARSE */
 
@@ -47,10 +48,12 @@ int		handle_file(char *filename, int type);
 
 /* BUILTINS */
 
-/* ENV*/
+/* ENV */
 char	*get_path_env(char **path_env);
 char	path_join(char *path_split, char *cmd_arg);
 
+/* ERROR */
+void	free_array(char **array);
 
 
 #endif
