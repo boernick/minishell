16bd5694939d690d2516222cf0ca852f097abb62
boernick <nick.boer@hotmail.com>
2024-10-28 23:26:51 +0100

    update2810

--- srcs/main/minishell.c
+++ srcs/main/minishell.c
@@ -6,7 +6,7 @@
 /*   By: nick <nick@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 19:53:08 by nboer             #+#    #+#             */
-/*   Updated: 2024/10/28 23:07:29 by nick             ###   ########.fr       */
+/*   Updated: 2024/10/28 23:26:44 by nick             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,13 @@
 int	main(int argc, char *argv[], char **envp)
 {
 	t_data shell; //option is to declare one big struct that keeps track of all data.
-	initstruct(&shell) //function to initiate the struct and set some parameters
+	
+	initstruct(&shell); //function to initiate the struct and set some parameters
 	//init envp ??
-	while (MINISHELL SHOULD NOT EXIT)
+	while (shell->exit == 1) //while no exit signal
 	{
 		//handle signals
-		minishell(&shell) // 
+		minishell(&shell) //run minishell
 	}
 	//free everything
 	return (0)
