26120afc2324d53ee1d54754ed9a2d09072f3120
Nick Boer <nboer@c3a2c10.42berlin.de>
2024-11-01 18:08:34 +0100

    update

--- README.txt
+++ README.txt
@@ -5,3 +5,19 @@ TO GET ALL THE SRCS *.C FILEPATHS FOR YOUR MAKEFILE: GO TO THE SRCS FOLDER IN TE
 (find . -type f -name "*.c" | sed 's|^\./|srcs/|') | xclip -selection clipboard
 
 PASTE CLIPBOARD IN MAKEFILE.
+
+
+***GIT***
+
+CREATE REPOSITORY
+...
+//TO ACCESS A BRANCH
+	git checkout -b <BRANCH_NAME>
+	code .
+// DO SOME CODING
+	git add .
+	git commit -m "custom message"
+	git push -u origin <BRANCH_NAME>
+// GO BACK TO MASTER
+	git checkout master
+	git pull
--- srcs/env/env.c
+++ srcs/env/env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nick <nick@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 21:20:22 by nick              #+#    #+#             */
-/*   Updated: 2024/10/30 23:39:49 by nick             ###   ########.fr       */
+/*   Updated: 2024/11/01 16:06:59 by nboer            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ int	t_env_init(t_data *shell, char **envp)
 	}
 	return (0);
 }
-// Adds a new environment variable `value` to the `envp` array, resizing it. 
+// Adds a new environment variable `value` to the `envp` list. 
 // Return int to succes or failure. */
 int	env_addback(t_data *shell, char *envp)
 {
@@ -70,12 +70,63 @@ int	env_addback(t_data *shell, char *envp)
 	current->next = new;
 	return (0);
 }
+// Deletes an environment variable from the list and
+// relinks the remaining nodes.
+int	env_del(t_data *shell, char *envp)
+{
+	t_env	*lst;
+	t_env	*prev;
+	int		l;
 
-void	env_del();
-
+	lst = shell->env_lst;
+	prev = NULL;
+	l = ft_strlen(envp);
+	while (lst) // for every node
+	{
+		if (!ft_strncmp(lst->content, envp, l) && lst->content[l] == '=')
+		{
+			if (prev)
+				prev->next = lst->next;
+			else // case head
+				shell->env_lst = lst->next;	
+			free(lst->content);
+			free(lst);
+			return (0);
+		}
+		prev = lst;
+		lst = lst->next;
+	}
+	return (-1);
+}
 //the linked list nodes need to be transformed into a continues string, but with each node
 // seperated by a new line
-void	env_getstr();
+char	*env_getstr(t_data *shell)
+{
+	t_env	*lst;
+	char	*str;
+
+	str = NULL;
+	lst = shell->env_lst;
+	while (lst)
+	{
+		if (!(str = ft_strjoin(str, lst->content)))
+		{
+			free(str);
+			return (NULL);
+		}
+		if (lst->next)
+		{
+			if (!(str = ft_strjoin(str, "\n")))
+			{
+				free(str);
+				return (NULL);
+			}
+		}
+		lst = lst->next;
+	}
+	return (str);
+}
+
 
 // Search through bin folder (NOW LINKED LIST) which dir contains PATH environment variable, 
 // skips "PATH" in the string and returns it.
@@ -85,7 +136,7 @@ char	*get_path_env(char **path_env)
 	while (path_env[i]) // TO DO THIS WILL BECOME LINKED LIST
 	{
 		if (ft_strnstr(path_env[i], "PATH", 4))
-			return path_env[i] + 5; 
+			return (path_env[i] + 5); 
 		i++;
 	}
 	return (NULL);
@@ -98,6 +149,8 @@ char	*path_join(char *path_split, char *cmd_arg)
 	char	*joined_path;
 
 	temp = ft_strjoin(path_split, "/");
+	if (!temp)
+
 	joined_path = ft_strjoin(temp, cmd_arg);
 	free(temp);
 	return (joined_path);
