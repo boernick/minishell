189243ecbad625aa1b29f65c05656d8fd9ff6d37
Nick Boer <nboer@c3a4c9.42berlin.de>
2024-11-03 13:45:31 +0100

    update

--- srcs/main/minishell.c
+++ srcs/main/minishell.c
@@ -6,44 +6,15 @@
 /*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 19:53:08 by nboer             #+#    #+#             */
-/*   Updated: 2024/11/02 17:09:49 by nboer            ###   ########.fr       */
+/*   Updated: 2024/11/03 13:45:21 by nboer            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "minishell.h"
 
-// this was the main of PIPEX.. but needs to read the amount of tokens and based on that create an N amount of pipes.
 void	minishell(char **argv, int argc, t_data *shell)
 {
-	int		i;
-	int		file_out;
-	int		file_in;
-	char	**env;
-		
-	env = envlst_to_array(shell);
-	i = 0;
-	while (i < argc)
-	{
-		// if (ft_strncmp("here_doc", argv[1], 8) == 0) //if case redirect = true
-		// {	
-		// 	if (argc < 6) 
-		// 		str_error("too little args"); //voor >> want: ./pipex here_doc LIMITER cmd cmd1 file zijn 6 args
-		// 	i = 3;
-		// 	file = handle_file(argv[argc - 1], 0);
-		// 	//here_doc(argv, env);
-		// }
-		// else
-		// {
-			i = 2;
-			file_in = handle_file(argv[1], 0);
-			file_out = handle_file(argv[argc - 1], 1);
-			dup2(file_in, STDIN_FILENO);
-		// }
-		while (i++ < argc - 2)
-			create_pipe(argv[i + 2], env);
-		dup2(file_out, STDOUT_FILENO);					//redirect STDOUT to file_out
-		run_ex(argv[argc + 2], env);					//run last ex.
-	}
+	..
 }
 
 int	main(int argc, char *argv[], char **envp)
