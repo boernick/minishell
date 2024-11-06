0d923b85e0e0955ba1dee0eb9036f7d85603e835
boernick <nick.boer@hotmail.com>
2024-11-07 00:35:38 +0100

    commit 0711

--- srcs/exec/ex_builtin.c
+++ srcs/exec/ex_builtin.c
@@ -6,7 +6,7 @@
 /*   By: nick <nick@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 18:11:28 by nboer             #+#    #+#             */
-/*   Updated: 2024/11/07 00:17:52 by nick             ###   ########.fr       */
+/*   Updated: 2024/11/07 00:35:16 by nick             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,21 @@ int	is_builtin(t_execution *pipex, char **argv)
 	(void) pipex;
 	if (!(ft_strncmp(argv[1], "echo", 4)))
 	{
-		if (!(ft_strncmp(argv[2], "n", 4)))
+		if (!(ft_strncmp(argv[2], "-n", 2)))
 			return (2);
 		return (1);
 	} 
-	if (!())
-	return (0);
+	if (!ft_strncmp(argv[1], "cd", 2))
+		return (3);
+	if (!ft_strncmp(argv[1], "pwd", 3))
+		return (4);
+	if (!ft_strncmp(argv[1], "unset", 5))
+		return (5);
+	return (-1);
 }
 
 // runs a given builtin function
 int	run_builtin(char* arg)
 {
-	(void) pipex;
 	return (0);
 }
